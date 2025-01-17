b856b949e69478c79169ae0b275bb18e19050fa2
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-01-17 23:14:23 +0300

    Calendar Extension + Tests
    Расширение для календаря, чтобы получать все недели для месяца

---
+++ FinanceApp/Model/Extensions/Calendar + Extensions.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+extension Calendar{
+    
+    func weekInMonth(of date: Date) -> [DateInterval]?{
+        
+        var weeks: [DateInterval] = []
+        
+        let startMonthComps = self.dateComponents([.year, .month], from: date)
+        guard var currentDate = self.date(from: startMonthComps) else { return nil }
+        
+        guard let range = self.range(of: .weekOfMonth, in: .month, for: date) else { return nil }
+        
+        for _ in range{
+            guard let weekInterval = self.dateInterval(of: .weekOfMonth, for: currentDate) else { continue }
+            weeks.append(weekInterval)
+            
+            guard let newDate = self.date(byAdding: .weekOfMonth, value: 1, to: currentDate) else { return nil }
+            currentDate = newDate
+        }
+
+        return weeks
+    }
+    
+}
---
+++ FinanceAppTests/CalendarExtensionTests.swift
@@ -0,0 +1,60 @@
+//
+//  DateExtensionTests.swift
+//  FinanceAppTests
+//
+//  Created by Данила Бондарь on 17.01.2025.
+//
+
+import XCTest
+@testable import FinanceApp
+
+final class CalendarExtensionTests: XCTestCase {
+
+    
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        try super.setUpWithError()
+    }
+
+    override class func tearDown() {
+        super.tearDown()
+    }
+    
+    func weekTest(date: Date){
+        
+        let weeks = Calendar.current.weekInMonth(of: date)
+        
+        XCTAssertNotNil(weeks)
+        XCTAssertEqual(weeks!.count, 5)
+        
+        XCTAssertEqual(weeks![0].start.timeIntervalSince1970, 1_735_506_000)
+        XCTAssertEqual(weeks![0].end.timeIntervalSince1970, 1_736_110_800)
+        
+        XCTAssertEqual(weeks![1].start.timeIntervalSince1970, 1_736_110_800)
+        XCTAssertEqual(weeks![1].end.timeIntervalSince1970, 1_736_715_600)
+        
+        XCTAssertEqual(weeks![2].start.timeIntervalSince1970, 1_736_715_600)
+        XCTAssertEqual(weeks![2].end.timeIntervalSince1970, 1_737_320_400)
+        
+        XCTAssertEqual(weeks![3].start.timeIntervalSince1970, 1_737_320_400)
+        XCTAssertEqual(weeks![3].end.timeIntervalSince1970, 1_737_925_200)
+        
+        XCTAssertEqual(weeks![4].start.timeIntervalSince1970, 1_737_925_200)
+        XCTAssertEqual(weeks![4].end.timeIntervalSince1970, 1_738_530_000)
+        
+        
+    }
+    
+    func testWeekGenerating() {
+        var date = Date(timeIntervalSince1970: 1_735_678_800)
+        weekTest(date: date)
+        
+        date = Date(timeIntervalSince1970: 1_736_888_400)
+        weekTest(date: date)
+        
+        date = Date(timeIntervalSince1970: 1_738_270_800)
+        weekTest(date: date)
+    }
+    
+}
