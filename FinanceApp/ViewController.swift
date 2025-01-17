b0b4471fc8c40dcc6d9097b717b5f17e81ea7b4d
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-01-17 19:27:34 +0300

    initial commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Данила Бондарь on 17.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
