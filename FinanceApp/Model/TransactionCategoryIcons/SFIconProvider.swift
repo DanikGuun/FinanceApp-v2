dd0a2c9e3d088b839059829ced6caa6f0b325b36
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2025-01-22 21:14:06 +0300

    SFIconProvider
    Добавил метод получения иконки по id

--- FinanceApp/Model/TransactionCategoryIcons/SFIconProvider.swift
+++ FinanceApp/Model/TransactionCategoryIcons/SFIconProvider.swift
@@ -1,24 +1,38 @@
 
 import UIKit
 
-final class SFIconProvider: TransactionCategoryIconProvider {
+final class SFIconProvider: TransactionCategoryIconProvider{
     
-    private let icons = [
-        SFCategoryIcon("plus", kind: .base)
+    private let icons: [any TransactionCategoryIcon] = [
+        SFCategoryIcon("plus", id: "plus", kind: .base)
     ]
     
     func getIcons() -> [any TransactionCategoryIcon] {
         return icons
     }
     
+    func getIcon(id: String) -> (any TransactionCategoryIcon)? {
+        return icons.first { $0.id == id }
+    }
+    
+    func getIconsWithType() -> [TransactionCategoryIconKind : [any TransactionCategoryIcon]] {
+        
+        var dict: [TransactionCategoryIconKind : [any TransactionCategoryIcon]] = [:]
+        icons.forEach { dict[$0.kind, default: []].append($0) }
+        return dict
+        
+    }
+    
 }
 
 private class SFCategoryIcon: TransactionCategoryIcon {
     
-    var kind: TransactionCategoryIconKind
-    var image: UIImage
+    let id: String
+    let kind: TransactionCategoryIconKind
+    let image: UIImage
     
-    init(_ imageName: String, kind: TransactionCategoryIconKind) {
+    init(_ imageName: String, id: String, kind: TransactionCategoryIconKind) {
+        self.id = id
         self.image = UIImage(systemName: imageName) ?? UIImage()
         self.kind = kind
     }
